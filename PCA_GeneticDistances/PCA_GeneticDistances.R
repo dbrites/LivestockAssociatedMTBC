b556060425d6bb8115c683853375e71926d0e240
dbrites <d.brites@unibas.ch>
2021-11-18 18:47:35 +0100

    Add files via upload

--- PCA_GeneticDistances/PCA_GeneticDistances.R
+++ PCA_GeneticDistances/PCA_GeneticDistances.R
@@ -384,3 +384,171 @@ table_2$Species3 <-factor(table_2$Species,levels=c("PZAsus-Af2",
 plot <-ggplot(table_2, aes(x = distances, y = Species3)) +
   geom_density_ridges(aes(fill = Species3))
 
+Following reviews a few duplicated sequences were identified withih La1. After removing duplicates genetic distances were re-calculated and plotted. 
+
+library(ape) 
+alignment738<- read.dna("boviscaprae_738_noduplicates_WGS.fasta_var_editedX.fasta",format="fasta")
+#remove outgroup
+alignment_737<-alignment738[rownames(alignment738) != "G08466",] dist737_pairwisedel<- dist.dna(alignment_737,model="N",variance=F,as.matrix=T,pairwise.deletion =T) 
+hist(dist737_pairwisedel) metadata 
+<-read.csv(file="Table1_corrected.txt",header=T,sep="\t",check.names=FALSE,stringsAsFactors = F) dim(metadata)
+#[1] 830 13 remove L6
+which(metadata$Sublineage=="L6")
+#[1] 462
+metadata <- metadata[-462,] dim(metadata)
+#[1] 829 12 remove orygis from metadata
+table(metadata$Sublineage) which(metadata$Sublineage=="La3") metadataLa1La2 <-metadata[-which(metadata$Sublineage=="La3"),] dim(metadataLa1La2)
+#[1] 738 13
+af2Gnumbers <-as.character(metadataLa1La2[metadataLa1La2$ClonalComplex_Loiseau2020=="Af2",5]) un2Gnumbers 
+<-as.character(metadataLa1La2[metadataLa1La2$ClonalComplex_Loiseau2020=="unknown2",5]) un3Gnumbers 
+<-as.character(metadataLa1La2[metadataLa1La2$ClonalComplex_Loiseau2020=="unknown3",5]) af1Gnumbers <- as.character(metadataLa1La2[metadataLa1La2$ClonalComplex_Loiseau2020=="Af1",5]) 
+un9Gnumbers <- as.character(metadataLa1La2[metadataLa1La2$ClonalComplex_Loiseau2020=="unknown9",5]) eu2Gnumbers <- 
+as.character(metadataLa1La2[metadataLa1La2$ClonalComplex_Loiseau2020=="Eu2",5]) un4Gnumbers <- as.character(metadataLa1La2[metadataLa1La2$ClonalComplex_Loiseau2020=="unknown4",5]) 
+un5Gnumbers <- as.character(metadataLa1La2[metadataLa1La2$ClonalComplex_Loiseau2020=="unknown5",5]) un7Gnumbers <- 
+as.character(metadataLa1La2[metadataLa1La2$ClonalComplex_Loiseau2020=="unknown7",5]) un6Gnumbers <- 
+as.character(metadataLa1La2[metadataLa1La2$ClonalComplex_Loiseau2020=="unknown6",5]) eu1Gnumbers <- as.character(metadataLa1La2[metadataLa1La2$ClonalComplex_Loiseau2020=="Eu1",5]) 
+pzaGnumbers <- as.character(metadataLa1La2[metadataLa1La2$Sublineage=="La1.1",5]) distPzaAf2 <- dist737_pairwisedel[rownames(dist737_pairwisedel) %in% pzaGnumbers, 
+rownames(dist737_pairwisedel) %in% af2Gnumbers] distPza <- dist737_pairwisedel[rownames(dist737_pairwisedel) %in% pzaGnumbers, rownames(dist737_pairwisedel) %in% pzaGnumbers] 
+distAf2un2<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% af2Gnumbers, rownames(dist737_pairwisedel) %in% un2Gnumbers] 
+distAf2<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% af2Gnumbers, rownames(dist737_pairwisedel) %in% af2Gnumbers] 
+distUn2<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% un2Gnumbers, rownames(dist737_pairwisedel) %in% un2Gnumbers] 
+distAf1un9<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% af1Gnumbers, rownames(dist737_pairwisedel) %in% un9Gnumbers] 
+distUn3un9<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% un3Gnumbers, rownames(dist737_pairwisedel) %in% un9Gnumbers] 
+distUn3Af1<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% un3Gnumbers, rownames(dist737_pairwisedel) %in% af1Gnumbers] distUn3 
+<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% un3Gnumbers, rownames(dist737_pairwisedel) %in% un3Gnumbers] distAf1<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% 
+af1Gnumbers, rownames(dist737_pairwisedel) %in% af1Gnumbers] distUn9<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% un9Gnumbers, rownames(dist737_pairwisedel) %in% 
+un9Gnumbers] distEu2un4<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% eu2Gnumbers, rownames(dist737_pairwisedel) %in% un4Gnumbers] 
+distUn5Un4<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% un5Gnumbers, rownames(dist737_pairwisedel) %in% un4Gnumbers] distUn5eu2 
+<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% un5Gnumbers, rownames(dist737_pairwisedel) %in% eu2Gnumbers] distEu2<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% 
+eu2Gnumbers, rownames(dist737_pairwisedel) %in% eu2Gnumbers] distUn4<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% un4Gnumbers, rownames(dist737_pairwisedel) %in% 
+un4Gnumbers] distUn5<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% un5Gnumbers, rownames(dist737_pairwisedel) %in% un5Gnumbers] distUn7eu1 
+<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% un7Gnumbers, rownames(dist737_pairwisedel) %in% eu1Gnumbers] distUn7un6 <-dist737_pairwisedel[rownames(dist737_pairwisedel) 
+%in% un7Gnumbers, rownames(dist737_pairwisedel) %in% un6Gnumbers] distUn6eu1 <-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% un6Gnumbers, rownames(dist737_pairwisedel) %in% 
+eu1Gnumbers] distEu1<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% eu1Gnumbers, rownames(dist737_pairwisedel) %in% eu1Gnumbers] 
+distUn6<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% un6Gnumbers, rownames(dist737_pairwisedel) %in% un6Gnumbers] 
+distUn7<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% un7Gnumbers, rownames(dist737_pairwisedel) %in% un7Gnumbers] 
+matrixDistAll=c(distPzaAf2,distPza,distAf2un2,distAf2,distUn2,distAf1un9,distUn3un9,distUn3Af1,distUn3,distAf1,distUn9,distEu2un4,distUn5Un4,distUn5eu2,distEu2,distUn4,distUn5,distUn7eu1,distUn7un6,distUn6eu1,distEu1,distUn6,distUn7) 
+matrixNameAll <-c(rep("PZAsus-Af2",length(distPzaAf2)),
+                rep("Pza",length(distPza)),
+                rep("Af2-Unk2",length(distAf2un2)),
+                rep("Af2",length(distAf2)),
+                rep("Unk2",length(distUn2)),
+                rep("Af1-Unk9",length(distAf1un9)),
+                rep("Unk3-Unk9",length(distUn3un9)),
+                rep("Unk3-Af1",length(distUn3Af1)),
+                rep("Unk3",length(distUn3)),
+                rep("Af1",length(distAf1)),
+                rep("Unk9",length(distUn9)),
+                rep("Eu2-Unk4",length(distEu2un4)),
+                rep("Unk5-Unk4",length(distUn5Un4)),
+                rep("Unk5-Eu2",length(distUn5eu2)),
+                rep("Eu2",length(distEu2)),
+                rep("Unk4",length(distUn4)),
+                rep("Unk5",length(distUn5)),
+                rep("Eu1-Unk7",length(distUn7eu1)),
+                rep("Unk7-Unk6",length(distUn7un6)),
+                rep("Eu1-Unk6",length(distUn6eu1)),
+                rep("Eu1",length(distEu1)),
+                rep("Unk6",length(distUn6)),
+                rep("Unk7",length(distUn7))) table <-data.frame(distances=length(matrixDistAll),Species=1,stringsAsFactors = FALSE) for (dist in 1:length(matrixDistAll)){
+  table[dist,1] =matrixDistAll[dist]
+  table[dist,2] =matrixNameAll[dist]
+}
+library(ggplot2) library(ggridges) table$Species <-factor(table$Species,levels=c("PZAsus-Af2",
+                                              "Af2-Unk2",
+                                              "Af1-Unk9",
+                                              "Unk3-Unk9",
+                                              "Unk3-Af1",
+                                              "Eu2-Unk4",
+                                              "Unk5-Unk4",
+                                              "Unk5-Eu2",
+                                              "Eu1-Unk7",
+                                              "Unk7-Unk6",
+                                              "Eu1-Unk6",
+                                              "Pza",
+                                              "Af2",
+                                              "Unk2",
+                                              "Unk3",
+                                              "Af1",
+                                              "Unk9",
+                                              "Eu2",
+                                              "Unk4",
+                                              "Unk5",
+                                              "Eu1",
+                                              "Unk6",
+                                              "Unk7" )) ggplot(table, aes(x = distances, y = Species)) +
+  geom_density_ridges(aes(fill = Species)) ggplot(table, aes(x = distances, y=Species)) +
+  geom_density_ridges(aes(fill=Species),)+
+  theme_classic()+
+  theme(legend.position = "none")+
+  labs(x = "SNP pair-wise distances")+
+  theme(axis.text.x = element_text(face="bold",size=12),axis.text.y = element_text(face="bold",size=12))
+##Create second factor to order the labels.
+table$Species2 <-factor(table$Species,levels=c("PZAsus-Af2",
+                                               "Pza",
+                                               "Af2-Unk2",
+                                               "Af2",
+                                               "Unk2",
+                                               "Unk3-Af1",
+                                               "Unk3-Unk9",
+                                               "Af1-Unk9",
+                                               "Unk3",
+                                               "Af1",
+                                               "Unk9",
+                                               "Eu2-Unk4",
+                                               "Unk5-Eu2",
+                                               "Unk5-Unk4",
+                                               "Eu2",
+                                               "Unk4",
+                                               "Unk5",
+                                               "Eu1-Unk7",
+                                               "Eu1-Unk6",
+                                               "Unk7-Unk6",
+                                               "Eu1",
+                                               "Unk6",
+                                               "Unk7" )) ggplot(table, aes(x = distances, y = Species2)) +
+  geom_density_ridges(aes(fill = Species2))
+#put un5 and un5 and Eu2 together in one group put un6 and un7 and Eu1 together in another group
+un5_4_eu2Gnumbers <-c(un4Gnumbers,un4Gnumbers,eu2Gnumbers) un6_7_eu1Gnumbers <-c(un6Gnumbers,un7Gnumbers,eu1Gnumbers) 
+distUn4un5Eu2_Un6un7Eu1<-dist737_pairwisedel[rownames(dist737_pairwisedel) %in% un5_4_eu2Gnumbers, rownames(dist737_pairwisedel) %in% un6_7_eu1Gnumbers] 
+matrixDistAll_2=c(distPzaAf2,distPza,distAf2un2,distAf2,distUn2,distAf1un9,distUn3un9,distUn3Af1,distUn3,distAf1,distUn9,distUn4un5Eu2_Un6un7Eu1,distEu2,distUn4,distUn5,distEu1,distUn6,distUn7) 
+matrixNameAll_2 <-c(rep("PZAsus-Af2",length(distPzaAf2)),
+                  rep("Pza",length(distPza)),
+                  rep("Af2-Unk2",length(distAf2un2)),
+                  rep("Af2",length(distAf2)),
+                  rep("Unk2",length(distUn2)),
+                  rep("Af1-Unk9",length(distAf1un9)),
+                  rep("Unk3-Unk9",length(distUn3un9)),
+                  rep("Unk3-Af1",length(distUn3Af1)),
+                  rep("Unk3",length(distUn3)),
+                  rep("Af1",length(distAf1)),
+                  rep("Unk9",length(distUn9)),
+                  rep("Unk4unk5Eu2-Unk6unk7Eu1",length(distUn4un5Eu2_Un6un7Eu1)),
+                  rep("Eu2",length(distEu2)),
+                  rep("Unk4",length(distUn4)),
+                  rep("Unk5",length(distUn5)),
+                  rep("Eu1",length(distEu1)),
+                  rep("Unk6",length(distUn6)),
+                  rep("Unk7",length(distUn7))) table_2 <-data.frame(distances=length(matrixDistAll_2),Species=1,stringsAsFactors = FALSE) for (dist in 1:length(matrixDistAll_2)){
+  table_2[dist,1] =matrixDistAll_2[dist]
+  table_2[dist,2] =matrixNameAll_2[dist]
+}
+table_2$Species3 <-factor(table_2$Species,levels=c("PZAsus-Af2",
+                                               "Pza",
+                                               "Af2-Unk2",
+                                               "Af2",
+                                               "Unk2",
+                                               "Unk3-Af1",
+                                               "Unk3-Unk9",
+                                               "Af1-Unk9",
+                                               "Unk3",
+                                               "Af1",
+                                               "Unk9",
+                                               "Unk4unk5Eu2-Unk6unk7Eu1",
+                                               "Eu2",
+                                               "Unk4",
+                                               "Unk5",
+                                               "Eu1",
+                                               "Unk6",
+                                               "Unk7" )) plot <-ggplot(table_2, aes(x = distances, y = Species3)) +
+  geom_density_ridges(aes(fill = Species3))
